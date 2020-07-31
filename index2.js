eff681e077db2f9fc5c5be790a4697b525cab8c8
rahcodes <39433922+rahcodes@users.noreply.github.com>
2020-07-31 17:26:59 +0400

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,34 @@
+function onJSONget(data)
+{
+    $("#output").html('<img src="" class="img-fluid" alt="" id="profilePic">');
+    $("#profilePic").attr("src", data.graphql.user.profile_pic_url_hd);
+
+    $("#search").attr("disabled", false);
+    $("#search").html("Search");
+    $("#username").val("");
+}
+
+function onJSONfail(data)
+{
+    $("#output").html('<div class="alert alert-danger alert-dismissible fade show" role="alert"> <strong>Error:</strong> Check the username and try again. <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">×</span> </button> </div>');
+
+    $("#search").attr("disabled", false);
+    $("#search").html("Search");
+}
+
+function search()
+{
+    let user = $("#username").val();
+
+    if (user)
+    {
+        $("#search").attr("disabled", true);
+        $("#search").html('<div class="spinner-border spinner-border-sm" role="status">');
+
+        let url = `https://instagram.com/${user}?__a=1`;
+
+        $.getJSON(url, onJSONget).fail(onJSONfail);
+    }
+}
+
+$("#search").click(search);
