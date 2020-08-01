27e8ad33caf3a74efdd03ec7f4d9575a32dd09c5
rahcodes <39433922+rahcodes@users.noreply.github.com>
2020-08-01 15:46:53 +0400

    Update index2.js

--- index2.js
+++ index2.js
@@ -2,6 +2,7 @@ function onJSONget(data)
 {
     $("#output").html('<img src="" class="img-fluid" alt="" id="profilePic">');
     $("#profilePic").attr("src", data.graphql.user.profile_pic_url_hd);
+    $("#profilePic").attr("download", data.graphql.user.username);
 
     $("#search").attr("disabled", false);
     $("#search").html("Search");
